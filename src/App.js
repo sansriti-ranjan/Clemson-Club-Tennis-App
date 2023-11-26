2b4cf825921ebb17c29e9ad70bc88f7f74900455
sansriti-ranjan <sanzuranjan@gmail.com>
2023-11-26 11:53:38 -0500

    Adding react files

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './app_login.cs'
+
+const App = () => {
+    return (
+        <div className='login-container'>
+            <h2>Login</h2>
+            <button className='signup-button'>Sign Up</button>
+            <button className='login-button'>Login</button>
+        </div>
+    );
+};
